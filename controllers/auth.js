3babfe5450367898900bbbe6dad31abb0043b4fb
Mayank <mayankindoliya@gmail.com>
2023-05-28 15:03:33 +0530

    if condition put in registerUser.

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,10 @@ const bcryptjs = require('bcryptjs');
 class authsController {
 
   static async registerUser(document) {
+    const existingUser = await Users.findOne({$or:[{name: document.name}, {emailId: document.emailId}]}).lean()
+    if(existingUser){
+      throw new Error('User Already exists !!!') 
+    }
     const salt = bcryptjs.genSaltSync(10)
     const hash = bcryptjs.hashSync(document.password, salt)
     document.password = hash
